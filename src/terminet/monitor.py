e05cbb9c320613a9cdbb2d132adfc995b5772b7d
no-kris <kris.tresk@gmail.com>
2025-03-17 15:36:01 -0400

    Update: Added method to Monitor class for sniffing network packets in a seperate thread. I also implemented a way to allow users to add their internet interface since that will be needed to access the internet. The default interface is eth0 but users can change it to a different one.

--- src/terminet/monitor.py
+++ src/terminet/monitor.py
@@ -1,5 +1,7 @@
+import asyncio
+import threading
 from textual.containers import HorizontalGroup
-from textual.widgets import Button
+from textual.widgets import Button, Input
 from scapy.all import IP, TCP, UDP, sniff
 from network_table import NetworkTable
 from textual.app import ComposeResult
@@ -14,12 +16,16 @@ class Monitor(HorizontalGroup):
         self.__network_table = network_table
         self.__sniffing_thread = None
         self.__running = False
+        self.__iface = Input(
+            placeholder="Enter your internet interface (e.g., eth0, wlan0)"
+        )
         self.__protocol_map = {
             1: "ICMP", 6: "TCP", 17: "UDP", 58: "ICMPv6", 132: "SCTP"
         }
 
     def compose(self) -> ComposeResult:
         """Create buttons for app."""
+        yield self.__iface
         yield Button("Start", id="start", variant="success")
         yield Button("Stop", id="stop", variant="error")
 
@@ -28,10 +34,34 @@ class Monitor(HorizontalGroup):
         btn_event = event.button.id
         if btn_event == "start":
             if not self.__running:
-                self.__running = True
+                asyncio.create_task(self.start_sniffing())
                 self.app.notify("Starting packet capture")
                 self.add_class("started")
         elif btn_event == "stop":
             self.__running = False
             self.app.notify("Stopping packet capture")
             self.remove_class("started")
+
+    async def start_sniffing(self) -> None:
+        """Start sniffing network packets in seperate thread."""
+        self.__running = True
+        self.__sniffing_thread = threading.Thread(
+            target=self.sniff_packets,
+            daemon=True
+        )
+        self.__sniffing_thread.start()
+
+    def sniff_packets(self) -> None:
+        """Sniff packets using Scapy sniff on the interface provided by the user."""
+        interface = self.__iface.value.strip()
+        try:
+            # Use specified interface if provided.
+            if interface:
+                self.app.notify(f"Sniffing on interface: {interface}")
+            else:
+                default_interface = "eth0"
+                self.app.notify(
+                    f"No interface specified, using default {default_interface}")
+        except Exception as e:
+            self.app.notify(f"Error: ", e)
+            self.__running = False
